e521fc79bbd73b7e7ce1e5e8fbf440281ebb0886
xhrist14n <xhrist14n@gmail.com>
2019-12-08 03:35:38 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ import './style.css';
 // Write TypeScript code!
 const message = 'Hola Mundo';
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = '<h1>'+message+'</h1>';
+appDiv.innerHTML = '<h1 class="display-1">'+message+'</h1>';
