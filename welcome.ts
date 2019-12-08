6387429d3d0b31d869680705d7863dde95c9541a
xhrist14n <xhrist14n@gmail.com>
2019-12-08 04:13:40 -0500

    1 file modified

--- welcome.ts
+++ welcome.ts
@@ -1,23 +1,23 @@
 // Import stylesheets
 import './style.css';
-
 import $ from "jquery";
-
 // ES6 Modules or TypeScript
 import Swal from 'sweetalert2'
-
-// CommonJS
-//const Swal = require('sweetalert2')
-
 // Write TypeScript code!
-let message:String = 'Proyecto Uno';
-message = message.toUpperCase();
 
 
-var html:string = '<h1 class="display-1 text-center text-primary text-uppercase">'+message+'</h1>';
+$(
+  function(){
 
-var appDiv = $("#app");
-appDiv.html(html);
+    let message:String = 'Proyecto Uno';
+    message = message.toUpperCase();
+    console.log(message);
+    var html:string = '<h1 class="display-1 text-center text-primary text-uppercase">'+message+'</h1>';
+    var appDiv = $("#app");
+    appDiv.html(html);
+
+  }
+);
 
 $(
   function(){
