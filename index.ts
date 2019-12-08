2ea77aa14ca74a2cb6a28463e22d4723aea85907
xhrist14n <xhrist14n@gmail.com>
2019-12-08 03:42:19 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -11,3 +11,10 @@ const Swal = require('sweetalert2')
 const message = 'Hola Mundo';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = '<h1 class="display-1 text-center text-primary">'+message+'</h1>';
+
+Swal.fire({
+  title: 'Error!',
+  text: 'Do you want to continue',
+  icon: 'error',
+  confirmButtonText: 'Cool'
+})
