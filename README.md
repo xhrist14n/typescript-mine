ea7e11ab614da288375c236019bdcdcd4572846d
xhrist14n <xhrist14n@gmail.com>
2019-12-08 03:31:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-mine
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-mine)
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const message = 'Hola Mundo';
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = '<h1>'+message+'</h1>';
