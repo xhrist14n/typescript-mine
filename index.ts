0762e8d74e12ac082aa7f2c2f0ff386c2d7cd5de
xhrist14n <xhrist14n@gmail.com>
2019-12-08 04:16:57 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
 // Import stylesheets
 import './style.css';
-
 import $ from "jquery";
-
 // ES6 Modules or TypeScript
 import Swal from 'sweetalert2'
-
-// CommonJS
-//const Swal = require('sweetalert2')
-
 // Write TypeScript code!
-const message = 'Hola Mundo';
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = '<h1 class="display-1 text-center text-primary">'+message+'</h1>';
 
 
+$(
+  function(){
+
+    let message:String = 'Proyecto Uno';
+    message = message.toUpperCase();
+    console.log(message);
+    var html:string = '<h1 class="display-1 text-center text-primary text-uppercase">'+message+'</h1>';
+    var appDiv = $("#app");
+    appDiv.html(html);
+
+  }
+);
 
 $(
   function(){
@@ -23,7 +26,7 @@ $(
         title: 'Exito!',
         text: 'Iniciando con Typescript',
         icon: 'success',
-        confirmButtonText: 'Exacto'
+        confirmButtonText: 'Preciso'
       }
     );
   }
